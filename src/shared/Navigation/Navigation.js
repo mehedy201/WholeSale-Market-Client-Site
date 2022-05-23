ca2c2ecdaca6d064c69c4960d8b6134745914844
mehedy201 <mehedy643@gmail.com>
2022-05-23 21:26:23 +0600

    install firebase hooks and added firebase init file

--- src/shared/Navigation/Navigation.js
+++ src/shared/Navigation/Navigation.js
@@ -27,10 +27,10 @@ const Navigation = () => {
         <div className="navbar p-0 bg-base-100 xl:max-w-[1140px] lg:max-w-[90%] md:max-w-[90%] sm:max-w-[90%] w-[90%] mx-auto">
             <div className="navbar p-0">
               <div className="dropdown lg:hidden">
-                <label tabindex="0" className="btn btn-ghost lg:hidden border border-slate-100">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                <label tabIndex="0" className="btn btn-ghost lg:hidden border border-slate-100">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                   {menuItems}
                 </ul>
               </div>
