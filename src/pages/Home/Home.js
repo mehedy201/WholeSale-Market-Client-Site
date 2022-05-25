d3904bcf41068223f5439fff61faa4e0393e8172
mehedy201 <mehedy643@gmail.com>
2022-05-25 13:56:45 +0600

    added product homepae

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'
 import heroImg from '../../images/heroimg.jpeg'
+import Product from '../Shop/Product';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+    console.log(products.slice(3));
     return (
         <div>
             <div className="hero min-h-screen">
@@ -15,6 +23,19 @@ const Home = () => {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-primary">Get Started</button>
                   </div>
+                </div>
+            </div>
+            <div>
+                <h2 className="text-2xl font-bold text-center mt-12 mb-6">Shop</h2>
+                <div className='my-12 grid grid-cols-3 gap-12'>
+                    {
+                        products.slice(3).map(product => 
+                            <Product
+                                key={product.id}
+                                product={product}
+                                ></Product>
+                        )
+                    }
                 </div>
             </div>
         </div>
