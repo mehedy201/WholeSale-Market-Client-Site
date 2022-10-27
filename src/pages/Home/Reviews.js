e080380a1d89797375cf400ba9996a22b7b1d04f
mehedy201 <mehedy643@gmail.com>
2022-10-27 17:10:36 +0600

    edit review section

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -1,11 +1,15 @@
+import { Rate } from 'antd';
 import React from 'react';
+import 'antd/dist/antd.css';
 
 const Reviews = ({review}) => {
     const {photoUrl, description} = review;
     return (
-        <div>
-            <img className='h-32 w-32 rounded-full border mx-auto' src={photoUrl} alt="" />
-            <p className='text-center'>{description}</p>
+        <div className=''>
+            <img className='h-24 w-24 p-2 shadow rounded-full border mx-auto outline-1 outline-green-500' src={photoUrl} alt="" />
+            <h2 className="text-sm text-center mt-2">Demo Name</h2>
+            <span className='flex justify-center'><Rate value={5} /></span>
+            <p className='text-center p-4 shadow rounded-xl mt-2'>{description}</p> 
         </div>
     );
 };
