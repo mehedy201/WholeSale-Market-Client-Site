091a5deb1bb636c8722d45f85208f03658636118
mehedy201 <mehedy643@gmail.com>
2022-10-27 16:34:26 +0600

    redesign company achivment section

--- src/pages/Home/AboutCompany.js
+++ src/pages/Home/AboutCompany.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import './AboutCompany.css'
+
 
 const AboutCompany = () => {
     return (
-        <>
-        <p className='text-4xl font-bold text-center mt-16 mb-6 text-primary'>Our Company Achievement</p>
-        <div className='my-12 grid grid-cols-1 lg:grid-cols-3 gap-12 bg-secondary py-16 rounded-lg'>
-            <div>
-                <p className='text-4xl font-bold text-center text-primary '>1000+</p>
-                <p className='text-2xl mt-2 font-semibold text-center'>Customers</p>
-            </div>
-            <div>
-                <p className='text-4xl font-bold text-center text-primary '>50k +</p>
-                <p className='text-2xl mt-2 font-semibold text-center'>Reviews</p>
-            </div>
-            <div>
-                <p className='text-4xl font-bold text-center text-primary '>100+</p>
-                <p className='text-2xl mt-2 font-semibold text-center'>Products</p>
+        <div className='for_background_image my-4'>
+            <div className='for_background_overly py-16'>
+                <p className='text-4xl font-bold text-center text-green-500'>Our Company Achievement</p>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-12 rounded-lg mt-8'>
+                    <div>
+                        <span className='flex justify-center font-bold text-2xl text-green-500'><CountUp duration={2.75} className='font-bold text-6xl text-white' end={1000} />+</span>
+                        <p className='text-2xl mt-2 font-semibold text-center text-white'>Customers</p>
+                    </div>
+                    <div>
+                    <span className='flex justify-center font-bold text-2xl text-green-500'><CountUp duration={2.75} className='font-bold text-6xl text-white' end={100} />K+</span>
+                        <p className='text-2xl mt-2 font-semibold text-center text-white'>Reviews</p>
+                    </div>
+                    <div>
+                        <span className='flex justify-center font-bold text-2xl text-green-500'><CountUp duration={2.75} className='font-bold text-6xl text-white' end={1000} />+</span>
+                        <p className='text-2xl mt-2 font-semibold text-center text-white'>Products</p>
+                    </div>
+                </div>
             </div>
         </div>
-        </>
     );
 };
 
